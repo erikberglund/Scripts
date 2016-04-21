561099e11caf89abbfa44090da0b12814aa4ed0b
Erik Berglund <erikberglund@Demo.local>
2016-04-21 21:31:41 +0200

    Added basic function: 'create_folder'
    Recursively create all folders passed to function
    Exists script if anything prevented folder creation

---
+++ functions/Bash/create_folder/README.md
@@ -0,0 +1,11 @@
+# create_folder
+
+
+Recursively create all folders passed to function.  
+Exists script if anything prevented folder creation.
+
+## Usage
+
+```bash
+create_folder "${path_folder_1}" "${path_folder_2}" ...
+```
---
+++ functions/Bash/create_folder/create_folder.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+### Version 1.0.0
+### Created by Erik Berglund
+### https://github.com/erikberglund
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+###
+### DESCRIPTION
+###
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+
+# Recursively create all folders passed to function.
+# Exists script if anything prevented folder creation.
+
+# Verifications.
+#   If folder path contains ^/Volumes, check that the mountpath exist before creating any folders recursively
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+###
+### USAGE
+###
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+
+# create_folder "${path_folder_1}" "${path_folder_2}" ...
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+###
+### FUNCTIONS
+###
+#//////////////////////////////////////////////////////////////////////////////////////////////////
+
+create_folder() {
+    for create_folder_folder in "${@}"; do
+        
+        # If folder path contains a mounted volume, check if volume is mounted before creating folder
+        if [[ ${create_folder_folder} =~ ^/Volumes ]]; then
+            local create_folder_folder_volume_mountpoint=$( awk -F"/" '{ print "/"$2"/"$3 }' <<< "${create_folder_folder}" )
+            if [[ ! -e "${create_folder_folder_volume_mountpoint}" ]] && [[ ! -d "${create_folder_folder_volume_mountpoint}" ]]; then
+                printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "Unable to create folder: ${create_folder_folder}"
+                printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "Mountpoint referenced in target path does not exist, will not create folder"
+                exit 1
+            fi
+        fi
+        
+        # Check if folder exists, else create it
+        if [[ -d ${create_folder_folder} ]]; then
+            if [[ -w ${create_folder_folder} ]]; then
+                printf "%s %s\n" "[${FUNCNAME}]" "Folder exist and current user ($( /usr/bin/id -un )) have write permissions."
+            else
+                printf "%s %s\n" "[${FUNCNAME}]" "Folder exist but current user ($( /usr/bin/id -un )) don't have write permissions."
+            fi
+        
+        # Check if folder path exists and is a file, exit with error
+        elif [[ -f ${create_folder_folder} ]]; then
+            printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "Unable to create folder: ${create_folder_folder}"
+            printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "A file already exist at path"
+            exit 1
+        else
+            create_folder_mkdir_output=$( /bin/mkdir -p "${create_folder_folder/#\~/$HOME}" 2>&1 )
+            if (( ${?} == 0 )); then
+                printf "%s %s\n" "[${FUNCNAME}]" "Folder '${create_folder_folder##*/}' was created successfully."
+            else
+                printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "Error creating folder: ${create_folder_folder}"
+                printf "%s %s\n" "[$( basename ${BASH_SOURCE[0]}):${FUNCNAME}:${LINENO}]" "$( /usr/bin/awk -F": " '{ print $3 }' <<< "${create_folder_mkdir_output}" )"
+                exit 1
+            fi
+        fi
+    done
+}
