48ead5f84a5b7fe9c4ef5d6f3133f8317697dfc1
Erik Berglund <erikberglund@Demo.local>
2016-10-14 08:56:36 +0200

    Moved ExtensionAttributes to own repository

--- extensionAttributes/printerSharingStatus.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if cups printer sharing is enabled on computer
-
-# Possible results: Enabled / Disable
-
-printf "%s%s%s\n" "<result>" "$( cupsctl | awk -F'=' '/_share_printers/ { if ($NF > 0) print "Enabled"; else print "Disabled" }' )" "</result>"
-
-exit 0
